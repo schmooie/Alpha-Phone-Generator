ddc43e1ce27cbed0a8a7fb731e2a99288b9abc1d
Sam Lau <slau234@gmail.com>
2014-05-17 18:54:27 -0400

    handling 1s and 0s

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,7 +6,7 @@ angular.module('phoneApp')
   	var wordLib = Words.getWords();
 
   	var alphaHash = {
-			1: ['1'],
+			1: ['l','i'],
 			2: ['a','b','c'],
 			3: ['d','e','f'],
 			4: ['g','h','i'],
@@ -15,7 +15,7 @@ angular.module('phoneApp')
 			7: ['p','q','r','s'],
 			8: ['t','u','v'],
 			9: ['w','x','y','z'],
-			0: ['0']
+			0: ['o']
 		};
 
 		var getCombos = function(arr, n) {
@@ -49,9 +49,21 @@ angular.module('phoneApp')
 
 		var findWord = function(num) {
 			var numLetters = [];
+			var zeroOrOneIdx = [];
 
-			num.split('').forEach(function(el) {
-				numLetters.push(alphaHash[el]);
+			num.split('').forEach(function(el, idx) {
+				if ( el === '0' || el === '1' ) {
+					var obj = {
+						letter: el,
+						index: idx
+					};
+
+					zeroOrOneIdx.push(obj);
+
+					numLetters.push(alphaHash[el]);
+				} else {
+					numLetters.push(alphaHash[el]);
+				}
 			});
 
 			var combos = getCombos(numLetters, numLetters.length);
@@ -64,14 +76,55 @@ angular.module('phoneApp')
 				}
 			});
 			console.log(validWords);
-			return validWords[Math.floor(Math.random() * validWords.length)];
+			var word = validWords[Math.floor(Math.random() * validWords.length)];
+
+			if ( typeof word === 'undefined' ) {
+				return;
+			} else {
+				word = word.split('');
+			}
+
+			// console.log(word);
+
+			zeroOrOneIdx.forEach(function(el) {
+				console.log(el);
+				word[el.index] = el.letter;
+			});
+
+			// console.log(word);
+
+			return word.join('').toUpperCase();
 		};
 
-    $scope.number = '244-277-2287';
+    $scope.number = '138-277-2287';
     $scope.convertNumber = function () {
-    	var number = $scope.number.replace(/[()-.]/g, "").replace(/\s/g,"");
+    	var number = $scope.number.replace(/[()-.]/g, '').replace(/\s/g, '');
 
-    	$scope.convertedNumber = findWord(number.slice(0,3)) + ' ' + findWord(number.slice(3,6)) + ' ' + findWord(number.slice(6));
+    	var numberSplits = [[0,3,6], [0,5], [0,6], [0,4]];
+
+    	var numberWords = '';
+
+    	for ( var i = 0; i < numberSplits.length; i++ ) {
+    		var splits = numberSplits[i];
+    		var count = 0;
+    		for ( var j = 1; j < splits.length; j++ ) {
+    			var word = findWord(number.slice(splits[j - 1], splits[j]));
+    			if ( typeof word === 'undefined' ) {
+    				break;
+    			} else {
+    				numberWords += word + ' ';
+    				count++;
+    			}
+    		}
+
+    		if ( count === splits.length ) {
+    			break;
+    		}
+    	}
+
+    	$scope.convertedNumber = numberWords;
+
+    	// $scope.convertedNumber = findWord(number.slice(0,3)) + ' ' + findWord(number.slice(3,6)) + ' ' + findWord(number.slice(6));
     	// $scope.convertedNumber = number; --- SET FINAL RESULT TO convertedNumber
     };
   });
